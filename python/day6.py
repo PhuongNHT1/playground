115f60328fbbe9f59d542eddd70312b2755410f9
PhuongNHT <phuongnht17@gmail.com>
2022-12-01 12:15:41 +0900

    play with list, map and filter

--- python/day6.py
+++ python/day6.py
@@ -6,3 +6,21 @@ number = [1,2,3,4,5,6,7,8,9,10,11,12]
 # When the two lists are of unequal length, the length of the shorter list is the length of the dictionary.
 month_dict = {key: value for (key, value) in zip(number, months)}
 print(month_dict)
+
+# set comprehension
+set_a = {x for x in range(10, 20) if x not in [12, 14, 16, 18]}
+print(set_a)
+
+for x in range (10,20):
+    print(x)
+
+a = [[96], [69]]
+print(map(str, a))
+print(''.join(list(map(str, a))))
+
+numbers = [15, 30, 47, 82, 95]
+def lesser(numbers):
+   return numbers < 50
+
+small = list(map(lesser, numbers))
+print(small)
